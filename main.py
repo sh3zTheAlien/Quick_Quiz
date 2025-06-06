2aa5d76fd466f6fc24daa56a59b158b011bb7b9f
sh3zTheAlien <ioannismrgl23@gmail.com>
2025-06-06 03:17:33 +0300

    Added Classes

--- main.py
+++ main.py
@@ -8,37 +8,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
+from quiz import Questions,Score
 import html
 import requests
-#https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple
+#https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=boolean
 
-def check_answers(user_answers,correct_answers):
-    user_correct = 0
-    user_average_score = 0
-    answered_questions = 0
-    user_false = 0
-    for i in range(len(correct_answers)):
-        if user_answers[i] == correct_answers[i]:
-            user_correct += 1
-    answered_questions = len(user_answers) # Doesnt matter if it is correct or user answers cuz we take the length.
-    user_average_score = (user_correct/answered_questions) * 100
-    user_false = answered_questions - user_correct
-    print(f"ANSWERED QUESTIONS:{answered_questions}\nAVERAGE SCORE: {user_average_score}%\n CORRECT ANSWERS:{user_correct}")
-
-def generate_questions():
-    response = requests.get("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=boolean")
-    if response.status_code == 200:
-        questions = response.json()["results"]
-        #correct_answer = bool(response.json()["results"][0]["correct_answer"])
-        return {"status":"success","questions":questions}
-    return {"status":"fail","error":f"STATUS:{response.status_code} JSON:{response.json()}"}
-
-def unescape_html(text:str):
-    return html.unescape(text)
+query_manager = Questions(amount=5,difficulty='medium',question_type='boolean',category=9)
+quiz_score = Score()
 
 #FLASK QUESTIONS
 app = Flask(__name__)
-app.jinja_env.filters['unescape'] = unescape_html
+app.jinja_env.filters['unescape'] = lambda text: html.unescape(text)
 
 @app.route("/")
 def home():
@@ -47,14 +27,13 @@ def home():
 
 @app.route("/quiz_game",methods=['GET','POST'])
 def game():
-    quiz = generate_questions()
+    quiz = query_manager.get_questions()
     if quiz["status"] == "success":
         if request.method == 'POST':
             user_answers = [request.form.get(f'answer{i}') for i in range(len(quiz["questions"]))]
             correct_answers = [x["correct_answer"] for x in quiz["questions"]]
-            print(f"User Answers: {user_answers}")
-            print(f"Correct Answers: {correct_answers}")
-            print(check_answers(user_answers,correct_answers))
+            print(quiz_score.check_answers(user_answers,correct_answers))
+            #check_answers(user_answers,correct_answers)
             return redirect(url_for('home'))
         return render_template("game.html",questions=quiz["questions"])
     return render_template("error.html",error=quiz["error"])
---
+++ quiz.py
@@ -0,0 +1,41 @@
+import requests
+
+class Questions:
+
+    def __init__(self,amount,difficulty,question_type,category):
+        """ type: 'bool' or 'multiple'
+            difficulty: 'easy' or 'medium' or 'hard'
+            amount: a number between 1 - 40
+            (questions)category: a number (video-games:15,sports:21,computers:18,general:9)"""
+        self.amount = amount
+        self.difficulty = difficulty
+        self.question_type = question_type
+        self.category = category
+
+    def get_questions(self):
+        response = requests.get(f"https://opentdb.com/api.php?amount={self.amount}&category={self.category}&difficulty={self.difficulty}&type={self.question_type}")
+        if response.status_code == 200:
+            return {"status":"success","questions":response.json()["results"]}
+        return {"status": "fail", "error": f"STATUS:{response.status_code} JSON:{response.json()}"}
+
+class Score:
+
+    def __init__(self):
+        self.score = 0
+        self.correct_answers = 0
+        self.answered_questions = 0
+        self.false_answers = 0
+        self.average_score = 0
+
+    def check_answers(self,user_answers,correct_answers):
+        for i in range(len(correct_answers)):
+            if user_answers[i] == correct_answers[i]:
+                self.correct_answers += 1
+
+        self.answered_questions = len(user_answers)
+        self.average_score = (self.correct_answers/self.answered_questions) * 100
+        self.false_answers = self.answered_questions - self.false_answers
+        return (f"ANSWERED QUESTIONS:{self.answered_questions}\n"
+                f"CORRECT ANSWERS:{self.correct_answers}\n"
+                f"FALSE ANSWERS:{self.false_answers}\n"
+                f"AVERAGE SCORE:{self.average_score}%")
